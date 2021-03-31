c39f9bc7c82e45c9a52dcdeb540f4a5dcc88c6b1
aymenkasP <ikoaymen056@gmail.com>
2021-03-31 20:30:18 +0100

    fix css

--- src/components/MatchLive.js
+++ src/components/MatchLive.js
@@ -1,20 +1,21 @@
 import { useStoreState } from 'easy-peasy'
 import React from 'react'
-
+import '../css/MatchLive.css'
 export default function MatchLive() {
     const info = useStoreState((state) => state.LiveInfo )
     const  emb = info.embed.slice(137,228)
     
     return (
-        <div>
+        <div className="video">
             <p> {info.title} </p>
-            <div>
-               {console.log(emb)}
-               <div className='video' ><iframe className='frame' src={emb} >
-               </iframe></div>
+            
+               <div className='frm' >
+               <iframe className='frame' src={emb} >
+               </iframe>
+               </div>
               
             </div>
-        </div>
+       
     )
 }
 
