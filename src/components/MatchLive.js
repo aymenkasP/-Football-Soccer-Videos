6236691422a4050dfc3e0428c92464900dbf3649
aymenkasP <ikoaymen056@gmail.com>
2021-03-31 19:38:07 +0100

    fin routing

--- src/components/MatchLive.js
+++ src/components/MatchLive.js
@@ -3,14 +3,19 @@ import React from 'react'
 
 export default function MatchLive() {
     const info = useStoreState((state) => state.LiveInfo )
+    const  emb = info.embed.slice(137,228)
     
-    console.log(info.embed)
     return (
         <div>
             <p> {info.title} </p>
             <div>
-               
+               {console.log(emb)}
+               <div className='video' ><iframe className='frame' src={emb} >
+               </iframe></div>
+              
             </div>
         </div>
     )
 }
+
+//230 132
